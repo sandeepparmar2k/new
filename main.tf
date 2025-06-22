b4e981450206a8ee83768b87cd38d71e229e384b
sandeepparmar2k <sandeep.parmar2k@gmail.com>
2025-06-22 19:21:02 +0530

    first upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+module "resource_group_module" {
+  source = "./modules/resource-group"
+  rg_name = "rg_main"
+  location = "East US"
+}
+module "storage_account_module" {
+  source = "./modules/storage-account"
+  resource_group_name = module.resource_group_module.name
+  location = module.resource_group_module.location1
+  vnet_name = "vnet1"
+  address_space = ["10.10.0.0/16"]
+  subnet_name = "subnet1"
+  address_prefixes = ["10.10.10.0/24"]
+  service_endpoints = ["Microsoft.Sql", "Microsoft.Storage"]           
+  storage_account_name = "sp00105storage"
+  account_tier = "Standard" 
+    account_replication_type = "LRS"
+    tags = {
+        environment = "staging"
+    }
+}
